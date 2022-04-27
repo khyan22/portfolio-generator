4727a1b9c05d9da4c4a3548015718618a6244651
khyan22 <khyanalvarez@gmail.com>
2022-04-26 22:24:57 -0400

    added project prompt

--- app.js
+++ app.js
@@ -10,14 +10,68 @@ const inquirer = require('inquirer')
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
 
+const promptUser = () => {
 
-inquirer
-  .prompt([
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?'
-    }
-  ])
-  .then((answers) => console.log(answers));
+    return inquirer.prompt([
+        { 
+            type: 'input',
+            name: 'fullName',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username'
+        },
+        {
+            type: 'input', 
+            name: 'about',
+            message: 'Provide some information about yourself:'
+        }
+    ]);
+};
+
+const promptProject = () => {
+    console.log(`
+=================
+Add a New Project
+=================
+`);
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ]);
+};
+
+promptUser()
+    .then(answers => console.log(answers))
+    .then(promptProject)
+    .then(projectAnswers => console.log(projectAnswers))
   
